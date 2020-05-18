176a94dbe4033813789b883115cd6ad8718e19c3
Ari Kardasis <ari.kardasis@gmail.com>
2020-05-18 09:09:56 -0400

    render time

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.get('/', (request, response) => {
   return response.json({
     data: {
-      message: `Testing changes`,
+      message: `the time on the server is ${Date.now()}`,
     },
   })
 })
