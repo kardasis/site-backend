0b96c0fdd929a7b8281d059556a731b57b63f7d1
Ari Kardasis <ari.kardasis@gmail.com>
2020-05-18 15:12:34 -0400

    add cors and axios

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (request, response) => {
   return response.json({
